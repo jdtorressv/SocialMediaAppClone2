4450fae188fbbc062f59b5d020b15193b476a742
Joseph Torres <jdtorres@ucdavis.edu>
2021-02-22 21:42:47 -0800

    thirty-five

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -1,36 +1,29 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
+const express = require('express');
+const app = express(); 
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello World!");
-});
+app.get('/tweets', (req, res) => {
+    admin.firestore().collection('tweets').get()
+    .then(data => {
+        let tweets = [];
+        data.forEach(doc => {
+            tweets.push(doc.data());
+        });
+        return res.json(tweets);
+    })
+    .catch(err => console.error(err))
+})
 
-exports.getScreams = functions.https.onRequest((req, res) => {
-    admin.firestore().collection("tweets").get()
-        .then(data => {
-            let tweets = [];
-            data.forEach(doc => {
-                tweets.push(doc.data());
-            });
-            return res.json(tweets);
-        })
-        .catch(err => console.error(err))
-});
-
-exports.createScream = functions.https.onRequest((req, res) => {
-    if (req.method !== 'POST') {
-        return res.status(400).json({error: "Method not permitted"});
-    }
+app.post('/tweet', (req, res) => {
     const newTweet = {
         body: req.body.body,
         userHandle: req.body.userHandle,
         createdAt: admin.firestore.Timestamp.fromDate(new Date())
     };
-    admin.firestore().collection("tweets").add(newTweet)
+    
+    admin.firestore().collection('tweets').add(newTweet)
     .then(doc => {
         res.json({message: `document ${doc.id} created successfully!`});
     })
@@ -39,3 +32,5 @@ exports.createScream = functions.https.onRequest((req, res) => {
         console.error(err);
     })
 });
+
+exports.api = functions.https.onRequest(app);
