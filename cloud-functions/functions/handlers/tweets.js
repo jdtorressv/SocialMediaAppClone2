0e8a7ac62dd1655c9a3125d6a9ad9d38ff2bcfe0
Joseph Torres <jdtorres@ucdavis.edu>
2021-02-27 21:18:57 -0800

    Commenting on posts

--- cloud-functions/functions/handlers/tweets.js
+++ cloud-functions/functions/handlers/tweets.js
@@ -36,3 +36,57 @@ exports.postOneTweet = (req, res) => {
         console.error(err);
     });
 }
+
+exports.getTweet = (req, res) => {
+    let tweetData = {};
+    db.doc(`/tweets/${req.params.tweetId}`)
+    .get()
+    .then((doc) => {
+        if (!doc.exists) {
+            return res.status(404).json({error: "Tweet could not be found"});
+        }
+        tweetData = doc.data();
+        tweetData.tweetId = doc.id;
+        return db
+        .collection('comments')
+        .orderBy('createdAt', 'desc')
+        .where('tweetId', '==', req.params.tweetId)
+        .get();
+    })
+    .then((data) => {
+        tweetData.comments = [];
+        data.forEach((doc) => {
+            console.log("Hi");
+            tweetData.comments.push(doc.data());
+        });
+        return res.json(tweetData);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).json({error: err.code});
+    });
+}
+
+// Comment on a tweet
+exports.commentOnTweet = (req, res) => {
+    if (req.body.body.trim() === '') return res.status(400).json({error: "Comment cannot be empty"});
+    const comment = {
+        body: req.body.body,
+        createdAt: new Date().toISOString(),
+        tweetId: req.params.tweetId,
+        userHandle: req.user.handle,
+        userImage: req.user.imageUrl
+    }
+    db.doc(`/tweets/${req.params.tweetId}`).get()
+    .then((doc) => {
+        if (!doc.exists) return res.status(404).json({error: "Tweet could not be found"});
+        return db.collection('comments').add(comment);
+    })
+    .then(() => {
+        res.json(comment);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).json({error: "Something went terribly wrong"});
+    });
+}
--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -3,12 +3,14 @@ const app = require('express')();
 
 const FBAuth = require('./util/fbAuth');
 
-const {getAllTweets, postOneTweet} = require('./handlers/tweets');
+const {getAllTweets, postOneTweet, getTweet, commentOnTweet} = require('./handlers/tweets');
 const {signup, login, uploadImage, addUserDetails, getAuthenticatedUser} = require('./handlers/users');
 
 // Tweet routes  
 app.get('/tweets', getAllTweets);
 app.post('/tweet', FBAuth, postOneTweet);
+app.get('/tweet/:tweetId', getTweet);
+app.post('/tweet/:tweetId/comment', FBAuth, commentOnTweet);
 
 //User routes
 app.post('/signup', signup);
