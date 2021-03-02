2380379396d3c6a8c853038fb09bd833f344427d
Joseph Torres <jdtorres@ucdavis.edu>
2021-03-01 22:17:10 -0800

    Started front end with react

---
+++ social-media-app-client/src/pages/signup.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class signup extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Sign In Page</h1>
+      </div>
+    );
+  }
+}
+
+export default signup;
