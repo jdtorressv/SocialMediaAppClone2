c25646b471f00c4eb355c9f236bf22bf280ab201
Joseph Torres <jdtorres@ucdavis.edu>
2021-03-22 20:48:51 -0700

    Five thirty-six thirty

--- social-media-app-client/src/pages/login.js
+++ social-media-app-client/src/pages/login.js
@@ -1,13 +1,104 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import AppIcon from '../images/icon.png';
+
+//MUI Stuff
+import {
+  withStyles,
+  Typography,
+  Grid,
+  TextField,
+  Button,
+} from '@material-ui/core';
+
+const styles = {
+  form: {
+    textAlign: 'center',
+  },
+  image: {
+    margin: '20px auto 20px auto',
+  },
+  pageTitle: {
+    margin: '10px auto 10px auto',
+  },
+  textField: {
+    margin: '10px auto 10px auto',
+  },
+  button: {
+    marginTop: 20,
+  },
+};
 
 export class login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      loading: 'false',
+      errors: {},
+    };
+  }
+
+  handleSubmit = () => {
+    console.log('hi there bud');
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <h1>Login Page</h1>
-      </div>
+      <Grid container className={classes.form}>
+        <Grid item sm />
+        <Grid item sm>
+          <img src={AppIcon} alt="Osprey Image" className={classes.image} />
+          <Typography variant="h2" className={classes.pageTitle}>
+            Login
+          </Typography>
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="email"
+              className={classes.textField}
+              value={this.state.email}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="password"
+              name="password"
+              type="password"
+              label="Password"
+              className={classes.textField}
+              value={this.state.password}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.button}
+            >
+              Login
+            </Button>
+          </form>
+        </Grid>
+        <Grid item sm />
+      </Grid>
     );
   }
 }
 
-export default login;
+login.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(login);
